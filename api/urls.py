ee8fad8295d8a78cf078d3fc99ff1f84d70b615e
Deepen <deepen@lintelindia.com>
2018-04-22 14:35:30 +0530

    Bug fixed for login and password reset.

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from .views import Signup, Login, TournamentView, GroundView, UserView
+from .views import Signup, Login, TournamentView, GroundView, UserView, LoginView, FPView
 
 urlpatterns = [
     url(r'^signup/$', Signup.as_view(), name='signup'),
-    url(r'^login/$', Login.as_view(), name='login'),
+    # url(r'^login/$', Login.as_view(), name='login'),
     url(r'^tournament/$', TournamentView.as_view(), name='tournament'),
     url(r'^ground/$', GroundView.as_view(), name='ground'),
     url(r'^users/$', UserView.as_view(), name='users'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^pwreset/$', FPView.as_view(), name='password reset'),
 ]
--- api/views.py
+++ api/views.py
@@ -5,6 +5,8 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+
 from .models import User, HostTournament, Ground
 from django.forms.models import model_to_dict
 from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
@@ -69,6 +71,102 @@ class UserView(mixins.ListModelMixin,
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+
+class LoginView(mixins.ListModelMixin,
+            mixins.RetrieveModelMixin,
+            mixins.UpdateModelMixin,
+            generics.GenericAPIView):
+    parser_classes = (JSONParser, MultiPartParser, FormParser, )
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+
+    # def get(self, request, *args, **kwargs):
+    #     queryset = User.objects.filter(email=request.query_params.get('email'), password=request.query_params.get('password')).values('name',
+    #                                                                           'email',
+    #                                                                           'password',
+    #                                                                           'phone_no',
+    #                                                                           'dob',
+    #                                                                           'address',
+    #                                                                           'special_skill',
+    #                                                                           'fav_game',
+    #                                                                           'profile',
+    #                                                                           )
+    #     print(queryset)
+    #     return Response(queryset, status=200)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            body = request.body.decode('utf-8')
+            body_data = json.loads(body)
+            queryset = User.objects.filter(email=body_data.get('email'),
+                                           password=body_data.get('password')).values('name',
+                                                                                 'email',
+                                                                                 'password',
+                                                                                 'phone_no',
+                                                                                 'dob',
+                                                                                 'address',
+                                                                                 'special_skill',
+                                                                                 'fav_game',
+                                                                                 'profile',
+                                                                                 )
+            print(queryset)
+            user_data = None
+            for u in queryset:
+                user_data = u
+            response = {
+                'status': 200,
+                'type': '+OK',
+                'user_data': user_data,
+                'message': 'Login Success',
+            }
+            return Response(response, status=200)
+        except Exception as e:
+            print(e)
+            response = {
+                'status': 500,
+                'type': '+OK',
+                'message': 'Login Failed',
+            }
+            return Response(response, status=500)
+
+
+class FPView(mixins.ListModelMixin,
+            mixins.RetrieveModelMixin,
+            mixins.UpdateModelMixin,
+            generics.GenericAPIView):
+    # parser_classes = (JSONParser, MultiPartParser, FormParser, )
+    # queryset = User.objects.all()
+    # serializer_class = UserSerializers
+
+    def post(self, request, *args, **kwargs):
+        try:
+            body = request.body.decode('utf-8')
+            body_data = json.loads(body)
+            queryset = User.objects.get(email=body_data.get('email'))
+            print(queryset)
+            if queryset:
+                queryset.password = body_data.get('password')
+                queryset.save()
+            print(queryset)
+            # user_data = None
+            # for u in queryset:
+            #     user_data = u
+            response = {
+                'status': 200,
+                'type': '+OK',
+                # 'user_data': user_data,
+                'message': 'Password reset',
+            }
+            return Response(response, status=200)
+        except Exception as e:
+            print(e)
+            response = {
+                'status': 500,
+                'type': '+OK',
+                'message': 'Password not able to reset',
+            }
+            return Response(response, status=500)
 
 
 class Login(View):
